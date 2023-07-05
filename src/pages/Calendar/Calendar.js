f64c42a3ad06344d5a3319cb8623a00fce902f77
sohan724code <sohanmd1375@gmail.com>
2023-07-05 21:55:20 +0600

    Update full calendar

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -1,7 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
+
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { tokens } from "../../theme";
+import Header from "../../components/Header/Header";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import { formatDate } from "@fullcalendar/core";
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  const handleDateClick = (selected) => {
+    const title = prompt("Please enter a new title for your event");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dataStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
+  };
+
+  const handleEventClick = (selected) => {
+    if (
+      window.confirm(
+        `Are you sure you sure you want to delete the event '${selected.event.title}'`
+      )
+    ) {
+      selected.event.remove();
+    }
+  };
+
+  return (
+    <Box sx={{ m: "20px" }}>
+      <Header title="CALENDAR" subtitle="full Calendar Interactive Page" />
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        {/* //TODO: Calendar Sidebar */}
+        <Box
+          sx={{
+            flex: "1 1 20%",
+            background: colors.primary[400],
+            p: "15px",
+            borderRadius: "4px",
+          }}
+        >
+          <Typography variant="h5"> Events</Typography>
+          <List>
+            {currentEvents?.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  background: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                ></ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        {/* //TODO: Calendar Section */}
+        <Box sx={{ flex: "1 1 100%", ml: "15px" }}>
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next,today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              { id: "1234", title: "All-day event", date: "2023-07-01" },
+              { id: "12314", title: "All-day event2", date: "2023-07-02" },
+              { id: "12324", title: "All-day event3", date: "2023-07-03" },
+              { id: "12334", title: "All-day event4", date: "2023-07-04" },
+              { id: "12344", title: "All-day event5", date: "2023-07-05" },
+            ]}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Calendar;
